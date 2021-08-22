8f8e00800e5dc3a38d7b187c66e8257deb3b15c8
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-08-22 12:51:57 -0600

    added log triggers and wallet update for transactions

--- algo-01-DB/algo Database SQL.sql
+++ algo-01-DB/algo Database SQL.sql
@@ -48,9 +48,8 @@ Create TABLE Portfolio
 (
     PortfolioNumber INTEGER CONSTRAINT PK_Portfolio PRIMARY KEY IDENTITY(1,1),
     Symbol VARCHAR(5) CONSTRAINT FK_port_Symbol FOREIGN KEY(Symbol) REFERENCES Stock_Item(Symbol),
-    PurchasePrice DECIMAL(10,4) CONSTRAINT CK_PurPrice CHECK(PurchasePrice > 0),
+    SalePrice DECIMAL(10,4) CONSTRAINT CK_PurPrice CHECK(SalePrice > 0),
     AmountOwned INTEGER CONSTRAINT CK_Portfolio_AmtOwned CHECK(AmountOwned >= 0),
-    --create trigger to remove if amountOwned < 0 
 )
 GO 
 
@@ -59,6 +58,7 @@ CREATE TABLE Wallet
     WalletNumber INTEGER CONSTRAINT PK_Wallet PRIMARY KEY IDENTITY(1,1),
     PortfolioNumber INTEGER CONSTRAINT FK_Portfolio FOREIGN KEY(PortfolioNumber) REFERENCES Portfolio(PortfolioNumber),
     CurrentBalance Decimal(15,2) CONSTRAINT CK_Wallet_Balance CHECK(CurrentBalance > 0),
+    LastTransactionDirection VARCHAR(4) CONSTRAINT CK_wallet_BuySell CHECK(LastTransactionDirection IN ('BUY', 'SELL'))
 
 )
 GO
@@ -95,7 +95,7 @@ INSERT INTO Stock_Item(Symbol, [Open], High, Low, [Close], Volume, DataTime)
 VALUES ('XXX', 420.69, 555.00, 69.00, 410.69, 1234, GETDATE()),
         ('YYY', 420.69, 555.00, 69.00, 410.69, 1234, GETDATE())
 
-INSERT INTO Portfolio(Symbol, PurchasePrice, AmountOwned)
+INSERT INTO Portfolio(Symbol, SalePrice, AmountOwned)
 VALUES ('XXX', 420.69, 15),
         ('YYY', 420.69, 0)
 
@@ -122,10 +122,80 @@ GO
 -- trigger to add to audit logs
 --market data trigger
 
+--TRIGGER MAY NOT HANDLE MULTIPLE INSERTS AT ONCE?? - REVIEW
+IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[StockItem_Update_LogAuditTrail]'))
+DROP TRIGGER StockItem_Update_LogAuditTrail
+GO
+
+CREATE TRIGGER StockItem_Update_LogAuditTrail
+ON Stock_Item
+FOR Insert, Update 
+AS
+    IF @@ROWCOUNT > 0 
+    BEGIN
+        INSERT INTO SYMBOL_HISTORY(Symbol, [Open], High, Low, [Close], Volume, DataTime)
+        SELECT I.Symbol, I.[Open], I.High, I.Low, I.[Close], I.Volume, I.DataTime
+        FROM inserted I
+
+        IF @@ERROR <> 0
+        BEGIN
+            RAISERROR('COULD NOT LOG SYMBOL HISTORY', 16,1)
+            ROLLBACK TRANSACTION
+        END 
+    END 
+    
+RETURN
+GO
+
 --wallet transaction trigger
+--updates current wallet balance as well as logs wallet history
+IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[Wallet_Update_LogAuditTrail]'))
+DROP TRIGGER Wallet_Update_LogAuditTrail
+GO
+
+CREATE TRIGGER Wallet_Update_LogAuditTrail
+ON Portfolio
+FOR Insert, Update 
+AS
+DECLARE @upd_Direction VARCHAR(5)
+    IF (select SalePrice from inserted) > 0
+        SET @upd_Direction = 'SELL'
+    ELSE
+        SET @upd_Direction = 'BUY'
+DECLARE @CurrentBalance_var  Decimal(15,2)
+        SET @CurrentBalance_var = (select CurrentBalance from Wallet where PortfolioNumber = (select PortfolioNumber from inserted) )
+
+    IF @@ROWCOUNT > 0 
+    BEGIN
+        INSERT INTO WALLET_HISTORY(PortfolioNumber, Balance, Symbol, Amount, Direction)
+        SELECT I.PortfolioNumber, (select CurrentBalance + I.SalePrice from Wallet where PortfolioNumber = I.PortfolioNumber ),
+                I.Symbol, I.SalePrice, @upd_Direction
+        FROM inserted I
+
+        
+        UPDATE Wallet
+        SET CurrentBalance = (@CurrentBalance_var + (select SalePrice from inserted)), LastTransactionDirection = @upd_Direction
+        WHERE PortfolioNumber = (select PortfolioNumber from inserted)
+
+        --check the balance of the wallet is > 0 
+        IF (select CurrentBalance from Wallet where PortfolioNumber = (select PortfolioNumber from inserted)) < 0
+        BEGIN
+            RAISERROR('insufficent funds', 16,1)
+            ROLLBACK TRANSACTION
+        END
+
+        IF @@ERROR <> 0
+        BEGIN
+            RAISERROR('COULD NOT LOG SYMBOL HISTORY', 16,1)
+            ROLLBACK TRANSACTION
+        END 
+    END 
+    
+RETURN
+GO
 
 
 -- create procs/trigs 
     --add purchase
     --get portfolio value
-    --audit log
+    --audit log INIT ON STARTUP
